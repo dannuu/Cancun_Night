48b4d7bd09e689e3df3e8e4b7e188be91a3bbd26
Dannuu <dannuu51@gmail.com>
2017-07-15 00:20:24 -0500

    Initial commit Class propiedades
    Class propiedades

---
+++ app/src/main/java/com/example/daniel/cancun_night/Models/Propiedades.java
@@ -0,0 +1,14 @@
+package com.example.daniel.cancun_night.Models;
+
+/**
+ * Created by daniel on 15/07/2017.
+ */
+
+public class Propiedades {
+
+    String name;
+    String descripcion;
+    String img_icon;
+
+
+}

2fb9d98bbca5604c8ed8a5d4916404ab034fa5b5
Dannuu <dannuu51@gmail.com>
2017-07-30 19:23:07 -0500

    Variables Carrousel
    Details

--- app/src/main/java/com/example/daniel/cancun_night/Models/Propiedades.java
+++ app/src/main/java/com/example/daniel/cancun_night/Models/Propiedades.java
@@ -9,17 +9,24 @@ public class Propiedades {
     String name;
     String descripcion;
     String img_icon;
+    String img_url;
+    String img_url_2;
+    String img_url_3;
 
 
     public Propiedades() {
     }
 
 
-    public Propiedades(String name,String descripcion, String img_icon ) {
+    public Propiedades(String name,String descripcion, String img_icon, String img_url,  String img_url_2, String img_url_3 ) {
 
         this.name = name;
         this.descripcion = descripcion;
         this.img_icon = img_icon;
+
+        this.img_url = img_url;
+        this.img_url_2 = img_url_2;
+        this.img_url_3 = img_url_3;
 
     }
 
@@ -38,6 +45,28 @@ public class Propiedades {
     public String getDescripcion() {return descripcion;}
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+
+
+    public String getImg_url() {
+        return img_url;
+    }
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public String getImg_url_2() {
+        return img_url_2;
+    }
+    public void setImg_url_2(String img_url_2) {
+        this.img_url_2 = img_url_2;
+    }
+
+    public String getImg_url_3() {
+        return img_url_3;
+    }
+    public void setImg_url_3(String img_url_3) {
+        this.img_url_3 = img_url_3;
     }
 
 
